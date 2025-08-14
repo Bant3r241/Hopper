7ec6bb4c7feccaa15a9aae0dabcc262d90388fec
Bant3r241 <bobby36ko@outlook.com>
2025-08-14 03:59:00 -0400

    Update Hop.lua

--- Hop.lua
+++ Hop.lua
@@ -4,12 +4,18 @@ local Players = game:GetService("Players")
 -- Function to server hop
 local function serverHop()
     -- Game ID for the game you're in
-    local gameId = 109983668079237  -- Replace this with your actual game ID if different
+    local gameId = 109983668079237  -- Your game place ID
 
-    -- Teleport to another random server in the same game
+    -- Attempt to get a list of available servers (you would typically use a service to get this list)
+    -- Since we don't have direct access to a list of servers, we'll assume teleporting works as expected.
+
+    -- You would normally get a list of servers through an API or pre-defined instances.
+    -- For now, we'll just teleport to the same game instance.
+
     local success, errorMessage = pcall(function()
-        -- Teleport to a random server in the same game using the TeleportService
-        TeleportService:Teleport(gameId, Players.LocalPlayer)
+        -- Teleport to a different instance of the same game using TeleportToPlaceInstance
+        -- Use the current game ID and job ID to join another server
+        TeleportService:TeleportToPlaceInstance(gameId, game.JobId, Players.LocalPlayer)
     end)
 
     -- If there's an error, print the error message
